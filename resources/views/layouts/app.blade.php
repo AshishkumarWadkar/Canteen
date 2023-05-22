bc0a707105fe78cf23192e24bc377045e9ac029d
dhananjay 0506 <dhananjay.j@reapmind.com>
2023-05-22 09:50:41 +0530

    ui changes home page and login page with api bind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,11 +15,29 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+
+    <link id="pagestyle" href="{{ asset('assets/css/material-dashboard.css') }}?v=3.1.0" rel="stylesheet">
+    <link href="{{ asset('assets/css/material-dashboard.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/material-dashboard.css.map') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/material-dashboard.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet"> --}}
+    {{-- css --}}
+    <link id="pagestyle" href="../public/assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -70,11 +89,80 @@
                     </ul>
                 </div>
             </div>
+        </nav> --}}
+        <nav
+            class="navbar navbar-expand-lg blur border-radius-xl top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+            <div class="container-fluid ps-2 pe-0">
+                <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
+                    Well to E-Canteen
+                </a>
+                <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon mt-2">
+                        <span class="navbar-toggler-bar bar1"></span>
+                        <span class="navbar-toggler-bar bar2"></span>
+                        <span class="navbar-toggler-bar bar3"></span>
+                    </span>
+                </button>
+                <div class="collapse navbar-collapse" id="navigation">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center me-2 active" aria-current="page"
+                                href="../pages/dashboard.html">
+                                <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link me-2" href="../pages/profile.html">
+                                <i class="fa fa-user opacity-6 text-dark me-1"></i>
+                                Profile
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link me-2" href="{{ route('register') }}">
+                                <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
+                                Sign Up
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link me-2" href="{{ route('login') }}">
+                                <i class="fas fa-key opacity-6 text-dark me-1"></i>
+                                Sign In
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+
+                        </li>
+                    </ul>
+                    {{-- <ul class="navbar-nav d-lg-flex d-none">
+                  <li class="nav-item d-flex align-items-center">
+                    <a class="btn btn-outline-primary btn-sm mb-0 me-2" target="_blank" href="https://www.creative-tim.com/builder?ref=navbar-material-dashboard">Online Builder</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="https://www.creative-tim.com/product/material-dashboard" class="btn btn-sm mb-0 me-1 bg-gradient-dark">Free download</a>
+                  </li>
+                </ul> --}}
+                </div>
+            </div>
         </nav>
 
-        <main class="py-4">
+        <main class="">
             @yield('content')
         </main>
     </div>
 </body>
+
 </html>
---
+++ resources/views/layouts/mess/app.blade.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    {{-- css --}}
+
+    <link  id="pagestyle"  href="{{ asset('assets/css/material-dashboard.css') }}?v=3.1.0" rel="stylesheet">
+    <link   href="{{ asset('assets/css/material-dashboard.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/material-dashboard.css.map') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/material-dashboard.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet"> --}}
+    {{-- css --}}
+    <link id="pagestyle" href="../public/assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+<script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+
+
+    <!-- Scripts -->
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+
+</head>
+<body>
+    <div id="app1" >
+
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+</body>
+<script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+<script src="{{ asset('assets/js/core/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('assets/js/core/bootstrap.min.js')}}"></script>
+<script src="{{ asset('assets/js/core/popper.min.js')}}"></script>
+<script src="{{ asset('assets/js/material-dashboard.js')}}"></script>
+{{-- <script src="{{ asset('assets/js/material-dashboard.js.map')}}"></script> --}}
+{{-- <script src="{{ asset('assets/js/material-dashboard.min.js')}}"></script> --}}
+
+<script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js')}}" ></script>
+<script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js')}}" ></script>
+
+</html>
